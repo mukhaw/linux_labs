ba66830a356de11784656f1dbc0a2257f60451e2
mukhaw <63058178+mukhaw@users.noreply.github.com>
2020-09-19 19:47:02 +0300

    Code from lab1

---
+++ lab1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{   
+    setlocale(LC_ALL, "Russian");
+    cout << "Муха Дарья Алексеевна "<< "\n";
+    cout << "7307" << endl;
+    cout << "Linux"<< endl;
+    return 0;
+}
